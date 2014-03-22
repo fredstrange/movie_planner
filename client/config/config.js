f6bbb85bf4f2fc10497c562860b7c691979c3986
James Rumack <james.rumack@videoplaza.com>
2014-03-22 18:14:35 +0100

    Fixed account config file to match new Account UI package.

--- client/config/config.js
+++ client/config/config.js
@@ -2,8 +2,10 @@ Accounts.ui.config({
     requestPermissions: {
         facebook: ['email', 'user_friends']
     },
+    extraSignupFields: []
+    /*,
 	requestOfflineToken: {
 		google: true
-	}//,
+	}*///,
 	//passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
